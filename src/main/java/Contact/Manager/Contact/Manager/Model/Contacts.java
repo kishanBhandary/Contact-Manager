e1f0aa8bfdb777ef7af166134294d7182d2173a6
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-17 20:37:43 +0530

    Add code

--- src/main/java/Contact/Manager/Contact/Manager/Model/Contacts.java
+++ src/main/java/Contact/Manager/Contact/Manager/Model/Contacts.java
@@ -4,15 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ContactList {
-      private List<Contacts> contacts;
-        private String Gmail;
-
-
-
-
+public class Contacts {
+    private String ContactName;
+    private String ContactNumber;
 }
--- src/main/java/Contact/Manager/Contact/Manager/Model/User.java
+++ src/main/java/Contact/Manager/Contact/Manager/Model/User.java
@@ -1,5 +1,4 @@
 package Contact.Manager.Contact.Manager.Model;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,18 +13,4 @@ public class User {
     @NonNull
     private String username;
     private String password;
-    private String email;
-    private String firstName;
-    private String lastName;
-    private String phoneNumber;
-    private String address;
-    private String city;
-    private String state;
-    private String zipCode;
-    private String country;
-
-
-
-
-
 }
