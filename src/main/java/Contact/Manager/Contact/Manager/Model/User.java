ac0fa69e3fcb472c20a2a24221a1f5151fe44efd
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-17 19:35:13 +0530

    Addede code

--- src/main/java/Contact/Manager/Contact/Manager/Model/User.java
+++ src/main/java/Contact/Manager/Contact/Manager/Model/User.java
@@ -1,4 +1,31 @@
 package Contact.Manager.Contact.Manager.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document("users")
 public class User {
+    @NonNull
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+
+
+
+
+
 }
