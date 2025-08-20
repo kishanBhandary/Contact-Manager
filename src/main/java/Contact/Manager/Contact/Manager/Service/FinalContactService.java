f1b3505a897f5d39288408df96677a78f7996647
kishanBhandary <kishanbhandary0@gmail.com>
2025-08-20 21:41:19 +0530

    Add code

--- src/main/java/Contact/Manager/Contact/Manager/Service/FinalContactService.java
+++ src/main/java/Contact/Manager/Contact/Manager/Service/FinalContactService.java
@@ -1,10 +1,9 @@
 package Contact.Manager.Contact.Manager.Service;
 
-import Contact.Manager.Contact.Manager.Model.FinalContact;
-import Contact.Manager.Contact.Manager.Model.PhoneNumberModel;
-import Contact.Manager.Contact.Manager.Model.User;
+import Contact.Manager.Contact.Manager.Model.*;
 import Contact.Manager.Contact.Manager.Repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators;
 
 public class FinalContactService {
 
@@ -12,6 +11,8 @@ public class FinalContactService {
     private ContactRepository contactRepository;
     @Autowired
     private UserService userService;
+
+
     public void addPhoneNumber(FinalContact finalContact, String userName, PhoneNumberModel phoneNumberModel) {
         User user = userService.findByUserName(userName);
         if(user != null) {
@@ -21,5 +22,34 @@ public class FinalContactService {
         else {
             throw new RuntimeException("User not found with username: " + userName);
         }
+    }
+
+
+    private void addGmail(FinalContact finalContact, String userName, GmailModel gmailModel){
+        User user = userService.findByUserName(userName);
+        if(user != null) {
+            finalContact.getGmail().add(gmailModel);
+            contactRepository.save(finalContact);
+        }
+        else {
+            throw new RuntimeException("User not found with username: " + userName);
+        }
+
+
+    }
+
+    private void addAddress(FinalContact finalContact, String userName, AddressModel addressModel) {
+        User user = userService.findByUserName(userName);
+        if(user != null) {
+            finalContact.getAddresses().add(addressModel);
+            contactRepository.save(finalContact);
+        }
+        else {
+            throw new RuntimeException("User not found with username: " + userName);
+        }
+    }
+
+    private void saveFinalContact(FinalContact finalContact) {
+        contactRepository.save(finalContact);
     }
 }
